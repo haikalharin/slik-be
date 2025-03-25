159e8375604e29ce1682ec241df6e129c5fa3648
haikal <haikalharin1998@gmail.com>
2025-03-25 19:56:53 +0700

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,74 +1,69 @@
 import {
-    Body,
-    Controller,
-    Post,
-    HttpCode,
-    HttpStatus,
-    Get,
-    Res, Req, UseGuards,
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  UseGuards,
 } from '@nestjs/common';
-import {AuthService} from './auth.service';
-import {ResponseUtil} from '../utils/response.util';
-import {User} from "../users/entities/user.entity";
-import {UsersService} from "../users/users.service";
-import {JwtAuthGuard} from "./entities/jwt-auth.guard";
+import { AuthService } from './auth.service';
+import { ResponseUtil } from '../utils/response.util';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { JwtAuthGuard } from './entities/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(
-        private authService: AuthService,
-        private usersService: UsersService,
-                ) {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() signInDto: Record<string, any>) {
+    const userData = await this.authService.signIn(
+      signInDto.username,
+      signInDto.password,
+    );
+
+    if (!userData) {
+      return ResponseUtil.error('Invalid credentials', HttpStatus.UNAUTHORIZED);
     }
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    async login(@Body() signInDto: Record<string, any>) {
-        const userData = await this.authService.signIn(
-            signInDto.username,
-            signInDto.password,
-        );
+    return ResponseUtil.success({
+      userId: userData.userId,
+      username: userData.username,
+      token: userData.token,
+    });
+  }
 
-        if (!userData) {
-            return ResponseUtil.error('Invalid credentials', HttpStatus.UNAUTHORIZED);
-        }
-
-        return ResponseUtil.success({
-            userId: userData.userId,
-            username: userData.username,
-            token: userData.token,
-        });
+  @Post('create')
+  async create(@Body() createUserDto: Partial<User>) {
+    // try {
+    const userExists = await this.usersService.checkUserExistsByUsername(
+      createUserDto.username,
+    );
+    if (userExists) {
+      return ResponseUtil.error('User Exist', HttpStatus.CONFLICT);
+    } else {
+      return this.authService.createUser(createUserDto);
     }
+    // }catch (error){
+    //     return ResponseUtil.error('Invalid credentials', HttpStatus.BAD_REQUEST);
+    //
+    // }
+  }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('getAllUser')
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
 
-    @Post('create')
-    async create(@Body() createUserDto: Partial<User>) {
-      // try {
-          const userExists = await this.usersService.checkUserExistsByUsername(createUserDto.username)
-          if (userExists) {
-              return ResponseUtil.error('User Exist', HttpStatus.CONFLICT);
-
-          } else {
-              return this.authService.createUser(createUserDto);
-          }
-      // }catch (error){
-      //     return ResponseUtil.error('Invalid credentials', HttpStatus.BAD_REQUEST);
-      //
-      // }
-
-
-
-    }
-
-    @UseGuards(JwtAuthGuard)
-    @Get('getAllUser')
-    async findAll(): Promise<User[]> {
-        return this.usersService.findAll();
-    }
-
-
-    @Get('getenv')
-    getEnv(): Promise<String> {
-      return this.authService.getDatabaseHost();
-    }
+  @Get('getenv')
+  getEnv(): Promise<string> {
+    return this.authService.getDatabaseHost();
+  }
 }
